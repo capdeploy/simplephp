3c42202485f1d0bcced402aeec4770a85368f75f
Brett Richter <brettrichter@nypl.org>
2024-05-22 14:08:14 -0400

    lets create a db init

---
+++ dbinit.php
@@ -0,0 +1,39 @@
+<?php
+$servername = "192.168.10.156";
+$username = "root";
+$password = "abcd1234";
+$dbname = "myDBPDO";
+
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE myDBPDO";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Database created successfully<br>";
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+    // sql to create table
+    $sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+  
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Table MyGuests created successfully";
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+?>
--- welcome.php
+++ welcome.php
@@ -29,4 +29,4 @@ try {
 }
 
 $conn = null;
-
+?>
